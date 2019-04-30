77a4291eb8028cebf12b613cd83a382e70f5cfbc
carlton anand <carlton.anand@gmail.com>
2019-04-30 17:41:35 +0100

    adding additional info to spec test file and the actual file in /lib

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -1,3 +1,6 @@
 class DockingStation
+  def release_bike
+    "hello biker"
+  end
 end
 
--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,5 +1,8 @@
 require 'docking_station'
 
 # In spec/docking_station_spec.rb
-describe DockingStation
+describe DockingStation do
+  it release_ bike do
+   expect(DockingStation).to respond_to(release_bike)
+  end
 end
